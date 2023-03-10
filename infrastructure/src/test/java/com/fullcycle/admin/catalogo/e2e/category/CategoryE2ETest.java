592736955133afc41ba6bdd62a26c87815a1d6ad
enermax626 <murilo.g.toloni@hotmail.com>
2023-03-10 20:28:42 -0300

    Last Changes before start working in another place

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/e2e/category/CategoryE2ETest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/e2e/category/CategoryE2ETest.java
@@ -1,17 +1,30 @@
 package com.fullcycle.admin.catalogo.e2e.category;
 
 import com.fullcycle.admin.catalogo.E2ETest;
+import com.fullcycle.admin.catalogo.domain.category.Category;
+import com.fullcycle.admin.catalogo.domain.category.CategoryId;
+import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @E2ETest
 @Testcontainers
+@AutoConfigureMockMvc
 public class CategoryE2ETest {
+
+  @Autowired
+  private MockMvc mvc;
+
+  @Autowired
+  private CategoryRepository categoryRepository;
 
   @Container
   public static final MySQLContainer MY_SQL_CONTAINER
@@ -27,7 +40,20 @@ public class CategoryE2ETest {
   }
 
   @Test
-  public void works() {
+  public void asCatalogAdminIShouldBeAbleToCreateACategoryWithValidValues() {
+    final var expectedName = "Filmes";
+    final var expectedDescription = "A categoria mais assistida";
+    final var expectedIsActive = true;
+
+
     Assertions.assertTrue(MY_SQL_CONTAINER.isRunning());
+    Assertions.assertEquals(0, categoryRepository.count());
   }
+
+
+  private CategoryId givenACategory(String name, String description, Boolean isActive) {
+    Category.newCategory()
+  }
+
+
 }
